794463113aa7c4fde16d20ca3d3463b138675981
xbowery <69230356+xbowery@users.noreply.github.com>
2021-11-27 11:38:05 +0800

    Initialise repository with Spring Initializr
    - adding required Spring Boot files from start.spring.io

---
+++ zendesk/zendesk/src/test/java/com/java/zendesk/ZendeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.zendesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZendeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
